f8ef0f5d2329e1339a015ecc364f516e60ce3698
kundan-72 <kundan7kundan@gmail.com>
2024-05-24 20:03:20 +0530

    rds included

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "elb_dns_name" {
   value = aws_elb.wordpress.dns_name
 }
 
-# output "rds_endpoint" {
-#   # value = aws_db_instance.main2.endpoint
-# }
+output "rds_endpoint" {
+  value = aws_db_instance.main2.endpoint
+}
--- rds.tf
+++ rds.tf
@@ -7,21 +7,21 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
-# resource "aws_db_instance" "main2" {
-#   identifier           = "wordpress-db"
-#   allocated_storage    = 20
-#   storage_type         = "gp2"
-#   engine               = "mysql"
-#   engine_version       = "8.0"
-#   instance_class       = "db.t3.micro"
-#   username             = "admin"
-#   password             = "password"
-#   db_subnet_group_name = aws_db_subnet_group.main.id
-#   multi_az             = true
-#   publicly_accessible  = false
-#   skip_final_snapshot  = true
+resource "aws_db_instance" "main2" {
+  identifier           = "wordpress-db"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "password"
+  db_subnet_group_name = aws_db_subnet_group.main.id
+  multi_az             = true
+  publicly_accessible  = false
+  skip_final_snapshot  = true
 
-#   tags = {
-#     Name = "wordpress-db"
-#   }
-# }
+  tags = {
+    Name = "wordpress-db"
+  }
+}
